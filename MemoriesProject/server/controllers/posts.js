619c5193759ddb3372bdaab67a832fb2f3ec8f78
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-22 22:26:21 +1100

    completed post get req middleware

--- MemoriesProject/server/controllers/posts.js
+++ MemoriesProject/server/controllers/posts.js
@@ -1,6 +1,22 @@
+import PostMessage from '../models/postMessage.js';
+
 // get posts middleware that corresponds to get router
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
     
+    try {
+        
+        // asynchronously get all posts
+        const postMessages = await PostMessage.find();
+        
+        console.log(postMessages);
+        
+        // posts were successfully retrieved
+        res.status(200).json(postMessages);
+    } catch (err) { 
+
+        // posts could not be found
+        res.status(404).json({ message: err.message });
+    }
 }
 
 // create posts middle that corresponds to the post router
