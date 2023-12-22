87ddfb45de741fefea9fe446a3cb582183f5b65f
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-23 01:52:05 +1100

    front end set up

--- MemoriesProject/client/src/App.js
+++ MemoriesProject/client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+// material ui components
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+
+// images
+import { memories } from './images/memories.png';
 const App = () => {
     
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth='lg'>
+            <AppBar position='static' color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"/>
+            </AppBar>
+        </Container>
     )
 }
 
