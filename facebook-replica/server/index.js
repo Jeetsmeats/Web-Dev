f56fb248d6a5662f337c23c332bbbf7836920af2
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-23 13:56:56 +1100

    server setup

---
+++ facebook-replica/server/index.js
@@ -0,0 +1,45 @@
+// import all dependencies
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+
+// allow setting of paths for configured directories
+import { fileURLToPath } from 'url';
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);  // to grab file url when using modules
+const __dirname = path.dirname(__filename); // when using type modules
+
+dotenv.config();
+
+// invoke express application
+const app = express();
+
+app.use(express.json());    // express middleware that parses JSON requests
+app.use(helmet());  // add helmet to middleware stack
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));  // blocks third party from using your resources from a cross origin
+app.use(morgan("common"));  // for logging requests, errors into the console
+app.use(bodyParser.json({ limit: "30mb", extended: true }));    // return middleware that only parses json 
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));      // return middleware with url encoded parsed bodies
+app.use(cors())     // add cors to middleware stack
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));  // sets the directory of all packages
+
+
+/* FILE STORAGE */
+// stores inputted files into the "public/assets" directory
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+// variable to upload files
+const upload = multer({ storage });
