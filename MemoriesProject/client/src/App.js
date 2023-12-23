7fd25631c53cac2c136b41d73e26f9ffcf6f2d96
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-23 13:10:50 +1100

    react folder structure

--- MemoriesProject/client/src/App.js
+++ MemoriesProject/client/src/App.js
@@ -3,8 +3,12 @@ import React from 'react';
 // material ui components
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
+// react components
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
 // images
-import { memories } from './images/memories.png';
+import memories from './images/memories.png';
 const App = () => {
     
     return (
@@ -13,6 +17,19 @@ const App = () => {
                 <Typography variant="h2" align="center">Memories</Typography>
                 <img src={memories} alt="memories" height="60"/>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between"
+                        alignItems="stretch" spacing="3">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     )
 }
