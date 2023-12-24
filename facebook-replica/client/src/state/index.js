3c5832a4514275d1bf39023efdb566356a3243e0
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-24 14:31:14 +1100

    create app logic with redux

---
+++ facebook-replica/client/src/state/index.js
@@ -0,0 +1,57 @@
+// import items
+import { createSlice } from "@reduxjs/toolkit";
+
+// global state accessible to the entire application
+const initialState = {
+
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {         // functions to change the state of the application
+        setMode: (state) => {       /* light/dark mode toggle */
+
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {      /* set the login information */
+
+            state.user = action.payload.user;   // payload sends the user parameter
+            state.token = action.payload.token; // payload sends the token parameter
+        },
+        setLogout: (state) => {     /* reset login information for logout */
+
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {        /* set friends for the user */
+
+            if (state.user) {   
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends are non existent -_-");
+            }
+        },
+        setPosts: (state, action) => {      /* set all posts in app */
+
+            state.posts = action.payload.posts;     
+        },
+        setPost: (state, action) => {       /* select post to return */
+            
+            const updatedPosts = state.posts.map((post) => {
+
+                if (post._id === action.payload._id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        }
+    }
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+
+export default authSlice.reducer;
