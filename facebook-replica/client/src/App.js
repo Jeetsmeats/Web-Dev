a0e4143d99e205eb7f8b0fc54d245d7a0b9d72a1
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-24 23:20:27 +1100

    set up themes

--- facebook-replica/client/src/App.js
+++ facebook-replica/client/src/App.js
@@ -6,17 +6,34 @@ import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
 
+// import for theme configuration
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+
 // application
 function App() {
-  
+
+  // get state from mode
+  const mode = useSelector((state) => state.mode);
+
+  // theme variable
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* CSS reset function */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
