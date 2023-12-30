a9a0bb114d386f02ca96329a6edfe68edad67bbb
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-30 14:05:27 +1100

    create state for the react app

---
+++ admin-website/client/src/state/index.js
@@ -0,0 +1,23 @@
+/* import tools */
+import { createSlice } from '@reduxjs/toolkit';
+
+/* initial app theme state */
+const initialState = {
+    mode: "dark"
+};
+
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setMode: (state) => {       // switch between light and dark
+
+            state.mode = state.mode === "light" ? "dark" : "light";
+        }
+    }
+});
+
+export const { setMode } = globalSlice.actions;
+
+export default globalSlice.reducer;
