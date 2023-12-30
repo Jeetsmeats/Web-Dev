13c8fe043898e4e5833ab038574574dcdcd6251f
Jeetsmeats <gunjeet.singh2@gmail.com>
2023-12-30 11:34:54 +1100

    index.js initial set up

---
+++ admin-website/server/index.js
@@ -0,0 +1,36 @@
+/* IMPORT DEPENDENCIES */
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+/* IMPORT ROUTES */
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+/* CONFIGURATIONS */
+
+// configure dotenv file with project
+dotenv.config();
+
+// expres app
+const app = express();
+
+// intialise app features
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use('/clients', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('management', managementRoutes);
+app.use('/sales', salesRoutes);
