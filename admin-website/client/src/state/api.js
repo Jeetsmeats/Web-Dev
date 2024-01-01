e30cb9ccc603f4907ddbf7a6c1ca3739c4b576a7
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-01-01 19:40:35 +1100

    get customers hook

--- admin-website/client/src/state/api.js
+++ admin-website/client/src/state/api.js
@@ -5,15 +5,19 @@ export const api = createApi({          // api for making calls to the backend
 
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User"],      // represent the state of particular data
+    tagTypes: ["User", "Products", "Customers"],      // represent the state of particular data
     endpoints: (build) => ({        // identify relevant api calls
-        getUser: build.query({
+        getUser: build.query({          /* GET USER HOOK */
             query: (id) => `general/user/${id}`,
             providesTags: ["User"]
         }),
-        getProducts: build.query({
+        getProducts: build.query({     /* GET PRODUCTS HOOK */
             query: () => "clients/products",
             providesTags: ["Products"]
+        }),
+        getCustomers: build.query({   /* GET CUSTOMERS HOOK */
+            query: () => "clients/customers",
+            providesTags: ["Customers"]
         })
     })
 })
@@ -21,4 +25,5 @@ export const api = createApi({          // api for making calls to the backend
 export const {
     useGetUserQuery,
     useGetProductsQuery,
+    useGetCustomersQuery,
 } = api;
