0cd6b53e00664fb1d98017800d9ef91a288d7fb0
Jeetsmeats <gunjeet.singh2@gmail.com>
2024-01-03 16:36:15 +1100

    get dashboard api endpoint complete

--- admin-website/server/controllers/general.js
+++ admin-website/server/controllers/general.js
@@ -1,5 +1,7 @@
 /* IMPORT FILES */
 import User from '../models/User.js';
+import OverallStat from '../models/OverallStat.js';
+import Transaction from '../models/Transaction.js';
 
 export const getUser = async (req, res) => {        // GET /users controller function
 
@@ -9,6 +11,53 @@ export const getUser = async (req, res) => {        // GET /users controller fun
         const user = await User.findById(id);      // find user by the request id
 
         res.status(200).json(user);         // user successfully found
+    } catch (error) {
+
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getDashboardStats = async (req, res) => {  // GET dashboard information
+
+    try {
+
+        // hardcoded data
+        const currentMonth = "November";
+        const currentYear = 2021;
+        const currentDay = "2021-11-15";
+
+        /* RECENT TRANSACTIONS */
+        const transactions = await Transaction.find().limit(50).sort({ createdOn: -1 });
+
+        /* OVERALL STATS */
+        const overallStat = await OverallStat.find({ year: currentYear });
+
+        const {
+            totalCustomers,
+            yearlyTotalSoldUnits,
+            yearlySalesTotal,
+            monthlyData,
+            saleByCategory
+        } = overallStat[0];
+
+        const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+            return month === currentMonth;
+        });
+
+        const todayStats = overallStat[0].dailyData.find(({ date }) => {
+            return date === currentDay;
+        });
+
+        res.status(200).json({
+            totalCustomers,
+            yearlyTotalSoldUnits,
+            yearlySalesTotal,
+            monthlyData,
+            saleByCategory,
+            thisMonthStats,
+            todayStats,
+            transactions,
+        });         // user successfully found
     } catch (error) {
 
         res.status(404).json({message: error.message});
--- admin-website/server/routes/general.js
+++ admin-website/server/routes/general.js
@@ -1,10 +1,10 @@
 /* IMPORT FILES */
 import express from 'express';
-import { getUser } from '../controllers/general.js';
+import { getUser, getDashboardStats } from '../controllers/general.js';
 
 // routes from express application
 const router = express.Router();
 
 router.get("/user/:id", getUser);   //  GET user 
-
+router.get("/dashboard", getDashboardStats)         // GET dashboard stats
 export default router;
